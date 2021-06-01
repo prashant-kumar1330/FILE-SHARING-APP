74d2d0c66308eebbadd4177ac0befb8b39c2ded1
prashant kumar <kumarprashant1330@gmail.com>
2021-06-01 19:18:51 +0530

    added drop container and image

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-// document.write("hello prashant ")
-// //alert("hh")
-// console.log("writing in console")
-// console.warn("too big")
-// VM1127:1 too big
-// (anonymous) @ VM1127:1
-// undefined
-// console.error("semicolon  missing")
-// VM1193:1 semicolon  missing
-// (anonymous) @ VM1193:1
-// undefined
-// console.assert(4==4)
-// undefined
-// console.assert("3==2")
-// undefined
-// console.assert(3=3)
-// VM1354:1 Uncaught SyntaxError: Invalid left-hand side in assignment
-// console.assert(4==4)
-// undefined
-// console.assert(2==3)
-// VM1437:1 Assertion failed: console.assert
-// (anonymous) @ VM1437:1
-// undefined
+const dropzone =document.querySelector(".drop-zone");
+dropzone.addEventListener(drag, (e)=>{
+    console.write("dra")
+})
