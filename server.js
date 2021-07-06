1a4e8bf96dc89f8abcb5ad5dc0d339810687cd16
prashant-kumar1330 <kumarprashant1330@gmail.com>
2021-07-06 18:46:19 +0530

    cors

--- server.js
+++ server.js
@@ -3,11 +3,32 @@ require('dotenv').config();
 const app= express();
 const path = require('path');
 require('dotenv').config();
+const cors = require('cors');
+const corsOptions = {
+    origin:  process.env.ALLOWED_CLIENTS.split(',')
+    // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+  }
+app.use(cors(corsOptions))
+app.use(express.static('public'));
 const PORT= process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(express.json());
 const connectDB = require('./config/db');
 connectDB();
+
+// Cors 
+
+
+// Default configuration looks like
+// {
+//     "origin": "*",
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+//   }
+
+
+app.use(express.static('public'));
 app.set('views', path.join(__dirname,'/views'));
 app.set('view engine' ,'ejs');
 
